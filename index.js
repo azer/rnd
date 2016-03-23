72b5ee4456767d66bad0020c188eae7f1fe8f8fb
Kes <kes@h3m3.com>
2016-03-22 18:30:28 -0700

    Update index.js
    Updated maximum integer  to the maximum per section 8.5 of the ECMA javascript spec.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = random;
 
 function random (max, min) {
-  max || (max = 999999999999);
+  // Maximum integer per ECMA javascript standard of 2^53 - 1
+  max || (max = 9007199254740991);
   min || (min = 0);
 
   return min + Math.floor(Math.random() * (max - min));
